d670be2dda072a51a67821791fd2fc66f2eae6cb
rohitware <rohitware99@gmail.com>
2023-01-20 20:57:01 +0530

    Create promptAssign.ts

---
+++ promptAssign.ts
@@ -0,0 +1,29 @@
+var mathMarks = Number(prompt('Enter Maths Marks'));
+console.log(`Maths Marks  = ${mathMarks}`);
+
+var phyMarks = Number(prompt('Enter Physics Marks'));
+console.log(`Physics Marks = ${phyMarks}`);
+
+var cheMarks = Number(prompt('Enter Chemistry Marks'));
+console.log(`Chemistry Marks = ${cheMarks}`);
+
+var totalMarks = mathMarks + phyMarks + cheMarks;
+var totalTofix = totalMarks.toFixed(2);
+console.log(`Total Marks:`, totalTofix);
+
+var averageMarks = totalMarks / 3;
+var averageTofix = averageMarks.toFixed(2);
+console.log(`Average Marks:`, averageTofix);
+
+if (averageMarks < 70) {
+    console.log(`C Grade`);
+}
+else if (averageMarks > 70 && averageMarks < 90) {
+    console.log(`B Grade`);
+}
+else if (averageMarks > 90 && averageMarks < 100) {
+    console.log(`A Grade`);
+}
+else {
+    console.log(`Invalid Data`);
+}
