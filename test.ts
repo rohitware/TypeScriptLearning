5cd83f4133996e62e09004cff97ccd174b363979
rohitware <rohitware99@gmail.com>
2023-01-20 20:59:42 +0530

    format doc

--- test.ts
+++ test.ts
@@ -3,9 +3,9 @@
 
 //confirm('Do you really want to do this?') ;
 
-var num1 = Number( prompt('Please enter first number'));
+var num1 = Number(prompt('Please enter first number'));
 
-var num2 = Number(prompt('Please enter second number')) ;
+var num2 = Number(prompt('Please enter second number'));
 
 
 console.log(num1 + num2);
