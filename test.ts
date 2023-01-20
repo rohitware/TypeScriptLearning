1020141988d8c7fbedad8837e433febda4dbfdd0
rohitware <rohitware99@gmail.com>
2023-01-20 20:56:52 +0530

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,15 @@
+//alert("Hello all");
+
+
+//confirm('Do you really want to do this?') ;
+
+var num1 = Number( prompt('Please enter first number'));
+
+var num2 = Number(prompt('Please enter second number')) ;
+
+
+console.log(num1 + num2);
+
+parseInt('32px'); // 32 
+
+Number('32dny'); //
