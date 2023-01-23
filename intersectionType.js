1954dad523e1457c5811158aa96a20b8aae70c97
rohitware <rohitware99@gmail.com>
2023-01-24 00:23:05 +0530

    Create intersectionType.js

---
+++ intersectionType.js
@@ -0,0 +1,3 @@
+function contact(details) {
+    console.log("Dear ".concat(details.name, ".I hope you have received our email at ").concat(details.email, ". We will call you at ").concat(details.phone, " shortly,"));
+}
