c23dba7466a99ba5e793c0292e311796455c890f
rohitware <rohitware99@gmail.com>
2023-01-24 00:23:16 +0530

    Update lexicalScope.ts

--- lexicalScope.ts
+++ lexicalScope.ts
@@ -1,24 +1,4 @@
 
-// *  alert();    
-// *  prompt();   
-
-// to get the information from user....
-
-alert("Hey please enter your Name");
-
-
-//confirm('Do you really want to do this?') ;
-
-var num1 = Number( prompt('Please enter first number'));
-
-var num2 = Number(prompt('Please enter second number')) ;
-
-
-console.log(num1 + num2);
-
-parseInt('32px'); // 32 
-
-Number('32dny'); //
 
 // ======================= Lexical Scope.....
 
