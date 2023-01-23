ca765482b8845fa79ea0b16477b4e67f0ab0721c
rohitware <rohitware99@gmail.com>
2023-01-24 00:22:52 +0530

    Create confirmPrompt.ts

---
+++ confirmPrompt.ts
@@ -0,0 +1,28 @@
+//alert("Hello all");/ / confirm("Do you really want to do this?");
+var num1 = Number(prompt("Please enter first number"));
+var num2 = Number(prompt("Please enter second number"));
+console.log(num1 + num2);
+parseInt("32px"); // 32Number('32dny'); //
+
+
+// *  alert();    
+// *  prompt();   
+
+// to get the information from user....
+
+alert("Hey please enter your Name");
+
+
+//confirm('Do you really want to do this?') ;
+
+var num1 = Number( prompt('Please enter first number'));
+
+var num2 = Number(prompt('Please enter second number')) ;
+
+
+console.log(num1 + num2);
+
+parseInt('32px'); // 32 
+
+Number('32dny'); //
+
