1ef10b2a9349f9abbeb601a7d1559cd2380c60e9
rohitware <rohitware99@gmail.com>
2023-01-25 18:49:49 +0530

    Update intersectionType.js

--- intersectionType.js
+++ intersectionType.js
@@ -1,3 +1,5 @@
 function contact(details) {
     console.log("Dear ".concat(details.name, ".I hope you have received our email at ").concat(details.email, ". We will call you at ").concat(details.phone, " shortly,"));
 }
+var objContact = { name: 'Rohit', email: 'rohit@gmail.com', phone: '9850979777' };
+contact(objContact); // string
